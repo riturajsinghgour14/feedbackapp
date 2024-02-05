5b8c84084006eacb663454496787cb8d0b8a392c
riturajsinghgour14 <riturajsinghgour8827@gmail.com>
2024-02-05 03:55:49 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,123 @@
+let form = document.querySelector("form");
+let select=document.querySelector("select");
+let textarea=document.querySelector("textarea");
+let ul=document.querySelector("ul");
+
+
+const saveFeedback = (e) => {
+    e.preventDefault();
+
+    let li = document.createElement("li");
+    let h1 = document.createElement("h1");
+    let h2 = document.createElement("h2");
+    let delBtn = document.createElement("button");
+
+    li.className = "list-group-item rounded-0 shadow-sm";
+    h1.className = "display-3";
+    h2.className = "display-4";
+    delBtn.className = "btn btn-sm btn-danger rounded-0 float-end";
+
+    h1.innerText ="Rating : " + select.value;
+    h2.innerText="Review : " + textarea.value;
+    delBtn.innerText="delete";
+  
+    li.appendChild(h1);
+    li.appendChild(h2);
+    li.appendChild(delBtn);
+    ul.appendChild(li);
+
+    form.reset();
+    
+};
+form.addEventListener("submit", saveFeedback);
+
+const deleteTodo = (e) =>{
+    if(e.target.className.includes ("btn-danger")){
+     let li = e.target.parentElement;
+     if(window.confirm("Are You Sure?")){
+        ul.removeChild(li);
+     }
+    } 
+};
+ul.addEventListener("click", deleteTodo);
+
+let clearBtn = document.querySelector("#clearBtn");
+  
+clearBtn.addEventListener("click", () => {
+let allListItems = document.querySelectorAll("li");
+
+allListItems.forEach((item) => {
+    ul.removeChild(item);
+});
+});
+
+
+// let clearBtn = document.querySelector("#clearBtn");
+
+// clearBtn.addEventListener("click", () => {
+//   let allListItems = document.querySelectorAll("li");
+
+//   allListItems.forEach((item) => {
+//     ul.removeChild(item);
+//   });
+// });
+
+// let form = document.querySelector("form");
+// let select = document.querySelector("select");
+// let textarea = document.querySelector("textarea");
+// let ul = document.querySelector("ul");
+
+// const saveFeedback = (e) => {
+//   e.preventDefault();
+
+//   //   Create Elements
+
+//   let li = document.createElement("li");
+//   let h1 = document.createElement("h1");
+//   let h2 = document.createElement("h2");
+//   let delBtn = document.createElement("button");
+
+//   //   Give Classes
+
+//   li.className = "list-group-item rounded-0 shadow-sm";
+//   h1.className = "display-3";
+//   h2.className = "display-4";
+//   delBtn.className = "btn btn-sm btn-danger rounded-0 float-end";
+
+//   //   Insert Text
+
+//   h1.innerText = "Rating : " + select.value;
+//   h2.innerText = "Review : " + textarea.value;
+//   delBtn.innerText = "Delete";
+
+//   li.appendChild(h1);
+//   li.appendChild(h2);
+//   li.appendChild(delBtn);
+
+//   ul.appendChild(li);
+
+//   form.reset();
+// };
+
+// form.addEventListener("submit", saveFeedback);
+
+// const deleteTodo = (e) => {
+//   if (e.target.className.includes("btn-danger")) {
+//     let li = e.target.parentElement;
+//     if (window.confirm("Are You Sure?")) {
+//       ul.removeChild(li);
+//     }
+//   }
+// };
+
+// ul.addEventListener("click", deleteTodo);
+
+// let clearBtn = document.querySelector("#clearBtn");
+
+// clearBtn.addEventListener("click", () => {
+//   let allListItems = document.querySelectorAll("li");
+
+//   allListItems.forEach((item) => {
+//     ul.removeChild(item);
+//   });
+// });
